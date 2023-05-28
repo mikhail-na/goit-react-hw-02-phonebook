2af8c749d1b5115d5df2e3ef8afa72e175597534
mikhail-na <115450336+mikhail-na@users.noreply.github.com>
2023-05-28 17:18:01 +0200

    added markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  
+  addContactToContacts = contact => {
+    const { contacts } = this.state;
+    const createContact = { id: nanoid(), ...contact };
+
+    contacts.some(({ name }) => name === contact.name)
+      ? alert(`${contact.name} is already in your contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, createContact],
+          
+      }));
+    
+  };
+
+  handleChangeFilter = ({ target: { value } }) => {
+    this.setState({ filter: value });
+  };
+
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalized = filter.toLowerCase();
+
+    return contacts.filter(({ name }) => name.toLowerCase().includes(normalized));
+  };
+
+   removeContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm addContact={this.addContactToContacts} />
+        </Section>
+        <Section title="Contacts">
+          <Filter
+            value={this.state.filter}
+            handleChangeFilter={this.handleChangeFilter} />
+          <ContactList
+            contacts={this.getFilteredContacts()}
+            removeContact={this.removeContact}
+          />
+        </Section>
+      </>
+    );
+  }
 };
